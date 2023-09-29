41d35600b1f1d96f82b2611dc36c8139e954aed5
codeLearnerrr <jeferson.eiji@icloud.com>
2023-09-28 21:47:17 -0300

    fix: preventing https://github.com/graphql-nexus/nexus/issues/1036

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,9 @@
 import { makeSchema } from "nexus";
 import { join } from 'path';
 import * as types from './graphql';
-import { GQLDate } from "./graphql/scalars/Date";
 
 export const schema = makeSchema({
-    types: [GQLDate, types],
+    types,
     outputs: {
         schema: join(process.cwd(), 'schema.graphql'),
         typegen: join(process.cwd(), 'nexus-typegen.ts')
